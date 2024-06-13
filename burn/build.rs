91da05f18174f34d37c636171f4ca8d83391a134
Tommy Smith <tommy@weaviate.io>
2024-06-13 16:03:09 +0100

    Clean up dir structure

--- burn/build.rs
+++
@@ -1,8 +0,0 @@
-use burn_import::onnx::ModelGen;
-
-fn main() {
-    ModelGen::new()
-        .input("onnx/models/flan-t5-base/model.onnx")
-        .out_dir("model")
-        .run_from_script();
-}
--- burn/model/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod vectorize {
-    include!(concat!(env!("OUT_DIR"), "/model/model.rs"));
-}
