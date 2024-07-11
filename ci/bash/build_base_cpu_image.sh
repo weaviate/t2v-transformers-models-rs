b9561b2a0d5232cd2fa366ca931149dbb314482d
Tommy Smith <tommy@weaviate.io>
2024-07-11 11:41:16 +0100

    Fix CUDA_VERSION reference in CPU script

--- ci/bash/build_base_cpu_image.sh
+++ ci/bash/build_base_cpu_image.sh
@@ -1,9 +1,9 @@
-docker build --build-arg CUDA_VERSION=$CUDA_VERSION -t base-cpu-${CUDA_VERSION} -f Dockerfile.base.cpu .
+docker build -t base-cpu -f Dockerfile.base.cpu .
 
 DOCKER_REGISTRY="ghcr.io/weaviate/t2v-transformers-models-rs"
 
-docker tag base-cpu-${CUDA_VERSION} $DOCKER_REGISTRY/base-cpu-${CUDA_VERSION}:latest
+docker tag base-cpu $DOCKER_REGISTRY/base-cpu:latest
 
-docker push $DOCKER_REGISTRY/base-cpu-${CUDA_VERSION}:latest
+docker push $DOCKER_REGISTRY/base-cpu:latest
 
 docker logout
