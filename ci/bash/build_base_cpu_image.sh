f3cc1c83a92a9f0d203b9ea8a07ca429b684d497
Tommy Smith <tommy@weaviate.io>
2024-07-11 12:13:37 +0100

    cache from ghcr hosted latest image

--- ci/bash/build_base_cpu_image.sh
+++ ci/bash/build_base_cpu_image.sh
@@ -1,9 +1,10 @@
-docker build -t base-cpu -f Dockerfile.base.cpu .
-
 DOCKER_REGISTRY="ghcr.io/weaviate/t2v-transformers-models-rs"
+IMAGE=$DOCKER_REGISTRY/base-cpu:latest
 
-docker tag base-cpu $DOCKER_REGISTRY/base-cpu:latest
+docker build --cache-from $IMAGE -t base-cpu -f Dockerfile.base.cpu .
 
-docker push $DOCKER_REGISTRY/base-cpu:latest
+docker tag base-cpu $IMAGE
+
+docker push $IMAGE
 
 docker logout
--- ci/bash/build_base_gpu_image.sh
+++ ci/bash/build_base_gpu_image.sh
@@ -1,9 +1,10 @@
-docker build --build-arg CUDA_VERSION=$CUDA_VERSION -t base-gpu-${CUDA_VERSION} -f Dockerfile.base.gpu .
-
 DOCKER_REGISTRY="ghcr.io/weaviate/t2v-transformers-models-rs"
+IMAGE=$DOCKER_REGISTRY/base-gpu-${CUDA_VERSION}:latest
 
-docker tag base-gpu-${CUDA_VERSION} $DOCKER_REGISTRY/base-gpu-${CUDA_VERSION}:latest
+docker build --cache-from $IMAGE --build-arg CUDA_VERSION=$CUDA_VERSION -t base-gpu -f Dockerfile.base.gpu .
 
-docker push $DOCKER_REGISTRY/base-gpu-${CUDA_VERSION}:latest
+docker tag base-gpu $IMAGE
+
+docker push $IMAGE
 
 docker logout
